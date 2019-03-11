b405e4ff078e7cc2ec88acbca46e3931fc48b567
Blessing Abobo <suspieabobo@yahoo.com>
2019-03-11 13:48:44 +0100

    chore(user): Set up coverall
    - Install nyc reporter tool
    - Install coveralls
    - Setup coveralls in my package.json file
    - run `npm test`
    
    [Delivers #164522266]

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './Server/routes/usersRoutes';
-
+import usersRouter from './Server/routes/usersRoutes';
 
 const app = express();
 const PORT = process.env.port || 8080;
@@ -9,7 +8,7 @@ const PORT = process.env.port || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/v1/auth', router);
+app.use('/api/v1/auth', usersRouter);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
