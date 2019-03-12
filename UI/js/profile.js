d725cab3947d0d4eecaf5bb45e0da3c35104c278
Blessing Abobo <suspieabobo@yahoo.com>
2019-03-12 11:02:34 +0100

    Modify Css files

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -1,6 +1,23 @@
+
+let viewFirstMessage = document.getElementById('viewFirstMessage');
+let viewSecondMessage = document.getElementById('viewSecondMessage');
+let viewThirdMessage = document.getElementById('viewThirdMessage');
+
+function view(){
+    let messageId = this.attributes['data-type'].value;
+    let message = document.getElementById(messageId);
+
+    message.classList.toggle('hide');
+}
+
+viewFirstMessage.addEventListener('click', view);
+viewSecondMessage.addEventListener('click', view);
+viewThirdMessage.addEventListener('click', view);
+
 let openNav = document.getElementById('openNav');
 let closeNav = document.getElementById('closeNav');
 let sideNav = document.querySelector('.sidenav');
+
 
 openNav.addEventListener('click', () => {
     sideNav.style.width = '80vw';
@@ -9,4 +26,12 @@ openNav.addEventListener('click', () => {
 closeNav.addEventListener('click', () => {
     sideNav.style.width='0';
 });
+
+
+
+
+
+
+
+
 
