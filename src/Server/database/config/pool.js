ccad01ff26af961d643a6f2b1474a85e767cdb0d
Blessing Abobo <suspieabobo@yahoo.com>
2019-03-19 11:57:56 +0100

    chore(user): Setup PostgreSQL - install pg modules - setup database connection
    [Delivers #164654928]

---
+++ src/Server/database/config/pool.js
@@ -0,0 +1,37 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const development = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+};
+
+const test = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE_TEST,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+};
+
+const production = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE_PRODUCTION,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+};
+
+let config;
+if (process.env.NODE_ENV === 'development') config = development;
+if (process.env.NODE_ENV === 'test') config = test;
+if (process.env.NODE_ENV === 'production') config = production;
+
+const pool = new pg.Pool(config);
+
+export default pool;
