0702d474c8fa033f740cc1f48b4e5e680134937f
Blessing Abobo <suspieabobo@yahoo.com>
2019-03-22 09:29:46 +0100

    Add Procfile

--- src/Server/database/config/pool.js
+++ src/Server/database/config/pool.js
@@ -19,7 +19,6 @@ const test = {
   port: process.env.PGPORT,
 };
 
-const production = postgres://bdcuzlctcqqapn:01f7edb32a851aca98bd9b9092360dffc2b9845dbf705ec2dc13a1de2a240bb4@ec2-107-20-177-161.compute-1.amazonaws.com:5432/d5qp19b9gv8htn;
 
 let config = development;
 if (process.env.NODE_ENV === 'test') config = test;
