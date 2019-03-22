d36ba5a2738d586ddc99dfad6b394c14d8a1a35d
Blessing Makaraba <39123908+Abobos@users.noreply.github.com>
2019-03-22 09:46:26 +0100

    update heroku branch

--- src/app.js
+++ src/app.js
@@ -11,8 +11,15 @@ const Port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/v1/auth', usersRouter);
-app.use('/api/v1', messagesRouter);
+app.get('/', (req, res) => res.json({ message: 'Welcome to EPIC Mail' }));
+
+app.use('/api/v1/auth', usersv1Router);
+app.use('/api/v1', messagesv1Router);
+app.get('/api/v1/docs', (req, res) => res.redirect('https://epicmail11.docs.apiary.io/#'));
+app.all('*', (req, res) => res.status(404).json({
+  status: '404',
+  error: 'This route is unavailable',
+}));
 
 app.listen(Port, () => {
   // eslint-disable-next-line no-console
