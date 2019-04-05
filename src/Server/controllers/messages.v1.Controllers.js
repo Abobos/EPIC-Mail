28201718b4d3c45b0d452f2db1a2ba928120e7ea
Blessing Abobo <suspieabobo@yahoo.com>
2019-04-05 07:13:22 +0100

    ft(messages): Completes fetch specific user's email endpoint : - write unit tests for endpoint - Structure endpoint to fetch specifi users email from database - Use Postman to test endpoint
    [Delivers #164805853]

--- src/Server/controllers/messages.v1.Controllers.js
+++ src/Server/controllers/messages.v1.Controllers.js
@@ -1,10 +1,9 @@
-import messages from '../database/messages.v1';
 import db from '../database/config/pool';
 import '@babel/polyfill';
 
 
 class messagesControllers {
-  static async sendMessage(req, res) {
+  static async sendEmail(req, res) {
     const senderId = req.decoded.userId;
     const { subject, message } = req.body;
     const { receiverId } = req.body;
@@ -18,8 +17,8 @@ class messagesControllers {
     });
   }
 
-  static async getReceivedMessage(req, res) {
-    const queryText = 'SELECT messages.id, inbox.createdOn, messages.subject, messages.message, messages.parentMessageId, inbox.senderId, inbox.receiverId, inbox.status FROM messages INNER JOIN inbox ON messages.id=inbox.messageId WHERE inbox.receiverId = $1 ORDER BY messages.id';
+  static async getReceivedEmails(req, res) {
+    const queryText = 'SELECT messages.id, inbox.createdOn, messages.subject, messages.message, inbox.senderId, inbox.receiverId, messages.parentMessageId, inbox.status FROM messages INNER JOIN inbox ON messages.id=inbox.messageId WHERE inbox.receiverId = $1 ORDER BY messages.id';
     const receivedMessages = await db.query(queryText, [req.decoded.userId]);
     await db.query('UPDATE inbox SET status = $1 WHERE receiverId = $2', ['read', req.decoded.userId]);
     if (receivedMessages.rows[0]) {
@@ -34,8 +33,8 @@ class messagesControllers {
     });
   }
 
-  static async getReceivedUnreadMessage(req, res) {
-    const queryStatement = 'SELECT messages.id, inbox.createdOn, messages.subject, messages.message, messages.parentMessageId, inbox.senderId, inbox.receiverId, inbox.status FROM messages INNER JOIN inbox ON messages.id=inbox.messageId WHERE inbox.receiverId = $1 AND inbox.status = $2 ORDER BY messages.id';
+  static async getUnreadEmails(req, res) {
+    const queryStatement = 'SELECT messages.id, inbox.createdOn, messages.subject, messages.message, inbox.senderId, inbox.receiverId, messages.parentMessageId, inbox.status FROM messages INNER JOIN inbox ON messages.id=inbox.messageId WHERE inbox.receiverId = $1 AND inbox.status = $2 ORDER BY messages.id';
     const unreadMessages = await db.query(queryStatement, [req.decoded.userId, 'unread']);
     if (unreadMessages.rows[0]) {
       return res.status(200).json({
@@ -49,8 +48,8 @@ class messagesControllers {
     });
   }
 
-  static async getSentMessage(req, res) {
-    const queryStatement = 'SELECT messages.id, sent.createdOn, messages.subject, messages.message, messages.parentMessageId, sent.senderId, sent.receiverId, sent.status FROM messages INNER JOIN sent ON messages.id=sent.messageId WHERE sent.senderId = $1 ORDER BY messages.id';
+  static async getSentEmails(req, res) {
+    const queryStatement = 'SELECT messages.id, sent.createdOn, messages.subject, messages.message, sent.senderId, sent.receiverId, messages.parentMessageId, sent.status FROM messages INNER JOIN sent ON messages.id=sent.messageId WHERE sent.senderId = $1 ORDER BY messages.id';
     const sentMessages = await db.query(queryStatement, [req.decoded.userId]);
     if (sentMessages.rows[0]) {
       return res.status(200).json({
@@ -64,43 +63,41 @@ class messagesControllers {
     });
   }
 
-  static getUserMessage(req, res) {
-    const messageId = Number(req.params.Id);
-    const getMessage = messages.find(m => m.id === messageId);
-    if (!getMessage) {
-      return res.status(404).json({
-        status: 'failed',
-        error: 'The message with the given id was not found',
+  static async getAnEmail(req, res) {
+    const messageId = Number(req.params.messageId);
+    const queryText = 'SELECT messages.id, inbox.createdOn, messages.subject, messages.message, inbox.senderId, inbox.receiverId, messages.parentMessageId, inbox.status FROM messages INNER JOIN inbox ON messages.id=inbox.messageId WHERE inbox.messageId = $1 AND inbox.receiverId = $2';
+    const EmailRecord = await db.query(queryText, [messageId, req.decoded.userId]);
+    if (EmailRecord.rows[0]) {
+      return res.status(200).json({
+        status: 'success',
+        data: EmailRecord.rows,
       });
     }
     return res.status(200).json({
       status: 'success',
-      data: [
-        getMessage,
-      ],
+      message: 'The email record with the given ID was not found',
     });
   }
 
-  static DeleteMessage(req, res) {
-    const messageId = Number(req.params.Id);
-    const getMessage = messages.find(m => m.id === messageId);
-    if (!getMessage) {
-      return res.status(404).json({
-        status: 'failed',
-        error: 'The message with the given id was not found',
-      });
-    }
-    const messageIndex = messages.indexOf(getMessage);
-    messages.splice(messageIndex, 1);
-    return res.status(200).json({
-      status: 'success',
-      data: [
-        {
-          message: getMessage.message,
-        },
-      ],
-    });
-  }
-}
+//   static async deleteAnEmail(req, res) {
+//     const messageId = Number(req.params.messageId);
+//     const deletedDetails = await db.query('DELETE FROM inbox WHERE id = $1 AND receiverId = $2 RETURNING *', [messageId, req.decoded.userId]);
+//     if (deletedDetails.rows[0]) {
+//       const deletedMessage = await db.query('SELECT message FROM messages WHERE id = $1', [messageId]);
+//       return res.status(200).json({
+//         status: 'success',
+//         data: [
+//           {
+//             message: deletedMessage.rows[0].message,
+//           },
+//         ],
+//       });
+//     }
+//     return res.status(200).json({
+//       status: 'success',
+//       message: 'The email record with the given ID was not found',
+//     });
+//   }
+ }
 
 export default messagesControllers;
--- src/Server/routes/messages.v1.Routes.js
+++ src/Server/routes/messages.v1.Routes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import messageControllers from '../controllers/messages.v1.Controllers';
-import validateMessageDetails from '../middlewares/validateMessageDetails';
+import { validateMessageDetails, validateMessageId } from '../middlewares/validateMessageDetails';
 import { verifyToken } from '../middlewares/tokenHandler';
 
 const router = express.Router();
 
-router.post('/messages', verifyToken, validateMessageDetails, messageControllers.sendMessage);
-router.get('/messages', verifyToken, messageControllers.getReceivedMessage);
-router.get('/messages/unread', verifyToken, messageControllers.getReceivedUnreadMessage);
-router.get('/messages/sent', verifyToken, messageControllers.getSentMessage);
-router.get('/messages/:Id', verifyToken, messageControllers.getUserMessage);
-router.delete('/messages/:Id', verifyToken, messageControllers.DeleteMessage);
+router.post('/messages', verifyToken, validateMessageDetails, messageControllers.sendEmail);
+router.get('/messages', verifyToken, messageControllers.getReceivedEmails);
+router.get('/messages/unread', verifyToken, messageControllers.getUnreadEmails);
+router.get('/messages/sent', verifyToken, messageControllers.getSentEmails);
+router.get('/messages/:messageId', verifyToken, validateMessageId, messageControllers.getAnEmail);
+// router.delete('/messages/:messageId', verifyToken, validateMessageId, messageControllers.deleteAnEmail);
 
 export default router;
