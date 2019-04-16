78bd18d51d2733d94d664c716d304d22d139467e
Blessing Abobo <suspieabobo@yahoo.com>
2019-04-16 20:26:53 +0100

    Fix heroku

--- Server/database/config/pool.js
+++ Server/database/config/pool.js
@@ -6,6 +6,7 @@ dotenv.config();
 let config = process.env.DATABASE_URL_DEV;
 
 if (process.env.NODE_ENV === 'test') config = process.env.DATABASE_URL_TEST;
+if (process.env.NODE_ENV === 'production') config = process.env.DATABASE_URL;
 
 const pool = new Pool({ connectionString: config });
 
