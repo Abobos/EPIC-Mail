68aabaeb5267433c4bbaf635c1ba861072faabdc
Blessing Abobo <suspieabobo@yahoo.com>
2019-04-25 14:30:16 +0100

    Change password reset link

--- Server/utils/sendEmail.js
+++ Server/utils/sendEmail.js
@@ -18,9 +18,9 @@ const sendEmail = async (email, firstname, token) => {
     subject: 'Here is the link to reset your password',
     html: `<p>Dear ${firstname},</p>
     <p>Click the link below to reset your password</p>
-    <p><a href='https://abobos.github.io/EPIC-Mail/UI/forgot_password.html?authorization=${token}'>Click here</a></p>
-    <p>If you have any problem using this link, you can click 
-    <a href='https://abobos.github.io/EPIC-Mail/UI/forgot_password.html?authorization=${token}'>
+    <p><a href='https://abobos.github.io/EPIC-Mail/UI/reset_password.html?authorization=${token}'>Click here</a></p>
+    <p>If you have any problem using this link, you can copy the direct link below: 
+    <a href='https://abobos.github.io/EPIC-Mail/UI/reset_password.html?authorization=${token}'>
     https://abobos.github.io/EPIC-Mail/UI/reset_password.html?authorization=${token}</a>
     <p>Please kindly note that the above link will expire within 24 hrs</p>`,
   };
