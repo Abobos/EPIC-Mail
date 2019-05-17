1e442c12d16799f01ec4703fc0bcff75cbf805db
Blessing Abobo <suspieabobo@yahoo.com>
2019-05-17 20:06:52 +0100

    Refactor token handler

--- Server/middlewares/tokenHandler.js
+++ Server/middlewares/tokenHandler.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = userDetails => jwt.sign(userDetails, process.env.SECRET_KEY, { expiresIn: '1d' });
+export const generateToken = userDetails => jwt.sign(userDetails, process.env.SECRET_KEY, { expiresIn: '1d' });
 
-const verifyToken = (req, res, next) => {
+export const verifyToken = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1] || req.headers.authorization;
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
@@ -16,4 +16,4 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-export { generateToken, verifyToken };
+// export { generateToken, verifyToken };
